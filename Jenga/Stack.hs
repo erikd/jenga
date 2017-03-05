9cafe201cead585c366d60cb4a1184a7e511f6d3
Erik de Castro Lopo <erikd@mega-nerd.com>
2017-03-05 16:32:44 +1100

    Read resolver version from stack.yaml

---
+++ Jenga/Stack.hs
@@ -0,0 +1,26 @@
+module Jenga.Stack where
+
+-- Read the resolver from the stack.yaml file in the current directory.
+-- This is unashamedly savage.
+
+import           Data.Char (isSpace)
+import qualified Data.List as DL
+
+import           Jenga.HTTP
+
+
+readResolver :: IO (Maybe StackResolver)
+readResolver = -- Is this the *only* file name?
+  extract <$> readFile "stack.yaml"
+
+
+extract :: String -> Maybe StackResolver
+extract xs =
+  case filter (DL.isPrefixOf "resolver:") (lines xs) of
+    [x] -> Just .StackResolver $ cleanup x
+    _ -> Nothing
+  where
+    cleanup = DL.takeWhile (not . isSpace)
+            . DL.dropWhile (isSpace)
+            . DL.drop 1
+            . DL.dropWhile (/= ':')
