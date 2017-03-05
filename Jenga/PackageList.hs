707a5513d61bb6c58f1549b1f3efe2f03a5a9eea
Erik de Castro Lopo <erikd@mega-nerd.com>
2017-03-05 15:39:31 +1100

    Convert the package list to a map

--- Jenga/PackageList.hs
+++ Jenga/PackageList.hs
@@ -1,31 +1,40 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Jenga.PackageList
-  ( Package (..)
+  ( PackageInfo (..)
   , PackageList (..)
   ) where
 
-import Data.Aeson
-import Data.Aeson.Types (typeMismatch)
-import Data.Text (Text)
+import           Data.Aeson
+import           Data.Aeson.Types (typeMismatch)
+
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as DM
+
+import           Data.Text (Text)
 
 
 data PackageList = PackageList
   { ghcVersion :: Text
   , creatDate :: Text
   , resolverName :: Text
-  , packageList :: [Package]
+  , packageList :: Map Text PackageInfo
   }
   deriving Show
 
 data Package = Package
-  { packageName :: Text
-  , packageVersion :: Text
-  , synopsis :: Text
-  , isCore :: Bool
+  { _pkgName :: Text
+  , _pkgVer :: Text
+  , _pkgSyn :: Text
+  , _pkgCCore :: Bool
+  }
+
+data PackageInfo = PackageInfo
+  { packageVersion :: Text
+  , packageSynopsis :: Text
+  , packageCore :: Bool
   }
   deriving Show
-
 
 -- Temporary data type. Not exported.
 data Snapshot = Snapshot
@@ -55,6 +64,13 @@ instance FromJSON PackageList where
   parseJSON (Object v) = do
     s <- v .: "snapshot"
     pkgs <- v .: "packages"
-    pure $ PackageList (ghc s) (created s) (name s) pkgs
+    pure $ PackageList (ghc s) (created s) (name s) $ packageMap pkgs
 
   parseJSON invalid = typeMismatch "PackageList" invalid
+
+packageMap :: [Package] -> Map Text PackageInfo
+packageMap =
+  DM.fromList . fmap convert
+  where
+    convert (Package nam ver syn core) =
+      (nam, PackageInfo ver syn core)
