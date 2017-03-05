808b3366bd88296303a0346e6b38c884978848a2
Erik de Castro Lopo <erikd@mega-nerd.com>
2017-03-05 17:34:00 +1100

    Jenga.Cabal: Remove duplicates

--- Jenga/Cabal.hs
+++ Jenga/Cabal.hs
@@ -8,13 +8,14 @@ module Jenga.Cabal
 -- Unfortunately its much more work than it should be. See:
 -- https://hackage.haskell.org/package/Cabal-1.24.2.0/docs/Distribution-PackageDescription.html#v:buildDepends
 
+import qualified Data.Map.Strict as DM
 import           Data.Text (Text)
 import qualified Data.Text as T
 
-import           Distribution.Package (Dependency (..), PackageName (..))
+import           Distribution.Package (Dependency (..), PackageIdentifier (..), PackageName (..))
 import           Distribution.PackageDescription
                     ( Benchmark, CondTree (..), ConfVar, Executable, GenericPackageDescription (..)
-                    , Library, TestSuite
+                    , PackageDescription (..), Library, TestSuite
                     )
 import           Distribution.PackageDescription.Parse (readPackageDescription)
 import           Distribution.Verbosity (normal)
@@ -23,11 +24,26 @@ import           Distribution.Verbosity (normal)
 readPackageDependencies :: FilePath -> IO [Dependency]
 readPackageDependencies fpath = do
   genpkg <- readPackageDescription normal fpath
-  pure $ extractLibraryDeps (condLibrary genpkg)
+  pure
+    $ sortNubByName
+    $ filterPackageName (package $ packageDescription genpkg)
+    $ extractLibraryDeps (condLibrary genpkg)
         ++ extractExecutableDeps (condExecutables genpkg)
         ++ extractTestSuiteDeps (condTestSuites genpkg)
         ++ extractBenchmarkDeps (condBenchmarks genpkg)
 
+
+sortNubByName :: [Dependency] -> [Dependency]
+sortNubByName = fmap toDep . DM.toList . DM.fromList . fmap fromDep
+  where
+    fromDep (Dependency n v) = (n, v)
+    toDep (n, v) = Dependency n v
+
+filterPackageName :: PackageIdentifier -> [Dependency] -> [Dependency]
+filterPackageName (PackageIdentifier pname _) =
+  filter (\dep -> pname /= packageName dep )
+  where
+    packageName (Dependency pn _) = pn
 
 dependencyName :: Dependency -> Text
 dependencyName (Dependency (PackageName name) _) = T.pack name
