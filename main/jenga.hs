a77eab431c7bbaf6150cd4fcad6a792ede15f2c2
Erik de Castro Lopo <erikd@mega-nerd.com>
2017-03-05 20:29:45 +1100

    Use optparse-applicative to parse command line
    And make mafia lock file output an option.

--- main/jenga.hs
+++ main/jenga.hs
@@ -4,6 +4,7 @@ import           Control.Monad (unless)
 
 import           Data.Either (partitionEithers)
 import qualified Data.List as DL
+import           Data.Monoid ((<>))
 import           Data.Text (Text)
 import qualified Data.Text.Lazy.IO as T
 
@@ -13,44 +14,68 @@ import           Jenga.PackageList
 import           Jenga.Render
 import           Jenga.Stack
 
+import           Options.Applicative (Parser, execParser, flag, help, info, long, short, strOption)
+
 import           System.IO (hPutStrLn, stderr)
-import           System.Environment (getArgs)
 
 
 main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    [cf] -> process cf
-    _ -> usageExit
+main =
+  execParser (info pCommand mempty) >>= process
 
+-- -----------------------------------------------------------------------------
 
-usageExit :: IO ()
-usageExit =
-  putStrLn $ "\nUsage: jenga <cabal file>\n"
+data OutputFormat
+  = OutputCabal
+  | OutputMafia
 
-process :: FilePath -> IO ()
-process cabalFile = do
-  deps <- fmap dependencyName <$> readPackageDependencies cabalFile
+data Command = Command FilePath OutputFormat
+
+pCommand :: Parser Command
+pCommand = Command <$> cabalFileP <*> outputFormatP
+
+cabalFileP :: Parser FilePath
+cabalFileP = strOption
+  (  short 'i'
+  <> long "input"
+  <> help "The input cabal file."
+  )
+
+outputFormatP :: Parser OutputFormat
+outputFormatP =
+  flag OutputCabal OutputMafia
+    (  short 'm'
+    <> long "mafia"
+    <> help "Render the output as a mafia lock file (rendering as a cabal.config is the default)."
+    )
+
+-- -----------------------------------------------------------------------------
+
+process :: Command -> IO ()
+process (Command cabalpath fmt) = do
+  deps <- fmap dependencyName <$> readPackageDependencies cabalpath
   mr <- readResolver
   case mr of
     Nothing -> putStrLn "Not able to find resolver version in 'stack.yaml' file."
-    Just r -> processResolver deps r
+    Just r -> processResolver fmt deps r
 
-processResolver :: [Text] -> StackResolver -> IO ()
-processResolver deps sr = do
+processResolver :: OutputFormat -> [Text] -> StackResolver -> IO ()
+processResolver fmt deps sr = do
   mpl <- getStackageResolverPkgList sr
   case mpl of
     Left s -> putStrLn $ "Error parse JSON: " ++ s
-    Right pl -> processPackageList deps pl
+    Right pl -> processPackageList fmt deps pl
 
 
-processPackageList :: [Text] -> PackageList -> IO ()
-processPackageList deps plist = do
+processPackageList :: OutputFormat -> [Text] -> PackageList -> IO ()
+processPackageList fmt deps plist = do
   let (missing, found) = partitionEithers $ lookupPackages plist deps
   unless (DL.null missing) $
     reportMissing missing
-  T.putStrLn . unLazyText $ renderAsCabalConfig found
+  T.putStrLn . unLazyText $
+    case fmt of
+      OutputCabal -> renderAsCabalConfig found
+      OutputMafia -> renderAsMafiaLock found
 
 
 reportMissing :: [Text] -> IO ()
