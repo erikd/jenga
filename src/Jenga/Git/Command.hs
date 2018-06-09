f6b8c2743e39a293cae38d9934f151159ee8c7f1
Erik de Castro Lopo <erikd@mega-nerd.com>
2018-06-09 11:18:00 +1000

    Jenga.Git.Command: Add gitHeadHash

--- src/Jenga/Git/Command.hs
+++ src/Jenga/Git/Command.hs
@@ -3,10 +3,17 @@
 module Jenga.Git.Command
   ( gitAddSubmodule
   , gitCheckoutCommit
+  , gitHeadHash
   , gitUpdate
   ) where
 
 import           Control.Monad.Trans.Either (EitherT)
+
+import           Data.Char (isSpace)
+import           Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text
+import qualified Data.Text.Encoding.Error as Text
 
 import           Jenga.Git.Process
 import           Jenga.Types
@@ -14,18 +21,29 @@ import           Jenga.Types
 
 gitAddSubmodule :: FilePath -> String -> EitherT JengaError IO ()
 gitAddSubmodule dest repo =
-  git ["submodule", "add", "--force", repo, dest]
+  gitHush ["submodule", "add", "--force", repo, dest]
 
 gitCheckoutCommit :: FilePath -> String -> EitherT JengaError IO ()
 gitCheckoutCommit dir hash =
-  git ["-C", dir, "checkout", hash]
+  gitHush ["-C", dir, "checkout", hash]
 
 gitUpdate :: FilePath -> EitherT JengaError IO ()
 gitUpdate dir = do
-  git ["-C", dir, "fetch"]
-  git ["-C", dir, "submodule", "update"]
+  gitHush ["-C", dir, "fetch"]
+  gitHush ["-C", dir, "submodule", "update"]
 
-git :: [Argument] -> EitherT JengaError IO ()
-git args = do
+gitHeadHash :: FilePath -> EitherT JengaError IO Text
+gitHeadHash dir =
+  gitOut ["-C", dir, "rev-parse", "HEAD"]
+
+gitHush :: [Argument] -> EitherT JengaError IO ()
+gitHush args = do
   Hush <- call (JengaGitError . renderProcessError) "git" args
   pure ()
+
+gitOut :: [Argument] -> EitherT JengaError IO Text
+gitOut args =
+  Text.takeWhile (not . isSpace)
+    . Text.decodeUtf8With Text.lenientDecode
+    . unOut
+    <$> call (JengaGitError . renderProcessError) "git" args
